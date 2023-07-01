21aa4d27d8b3eb6bda7709e9bd56a85f9e81fe1c
Ellen Samanta <ellen.samanta@outlook.com>
2023-07-01 20:26:33 -0300

    feat: criado conteudo de dicas para recomendações

---
+++ front/src/pages/Recomendacoes/dicas.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './styles/dicas.css';
+
+const DicasSuspeitaCancerBucal = () => {
+  return (
+    <div className="dicas-container">
+      <h2>O que fazer em caso de suspeita de câncer bucal?</h2>
+      <p>
+        Se você suspeita de câncer bucal, é importante tomar algumas medidas imediatas:
+      </p>
+      <ul>
+        <li>Procure um dentista ou médico especialista em estomatologia para uma avaliação</li>
+        <li>Não ignore os sintomas, como feridas na boca que não cicatrizam, dor persistente, entre outros</li>
+        <li>Informe ao profissional sobre seu histórico de saúde e hábitos, como tabagismo e consumo de álcool</li>
+        <li>Realize os exames solicitados pelo profissional para um diagnóstico preciso</li>
+        <li>Mantenha uma alimentação saudável e evite fatores de risco que possam estar relacionados ao câncer bucal</li>
+        <li>Procure apoio emocional e compartilhe suas preocupações com familiares e amigos</li>
+      </ul>
+    </div>
+  );
+}
+
+export default DicasSuspeitaCancerBucal;
