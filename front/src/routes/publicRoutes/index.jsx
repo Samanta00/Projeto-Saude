cf85349494cdb6fe443914ce0a3caab7f2550952
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-07-03 15:02:23 -0300

    fix

--- front/src/routes/publicRoutes/index.jsx
+++ front/src/routes/publicRoutes/index.jsx
@@ -3,12 +3,13 @@ import { Route } from 'react-router-dom';
 import PagesHome from '../../pages/Home/home';
 import PagesLogin from '../../pages/Login/login';
 import Cadastro from '../../pages/Cadastro/updateRegistration/form'
-
+import Quiz from '../../pages/Quiz';
 
 export const PublicRoutes = () => (
   <React.Fragment>
     <Route path="/" element={<PagesLogin />} />
     <Route path='/create' element={<Cadastro/>}/>
+    <Route path='/quiz' element={<Quiz/>}/>
   </React.Fragment>
 )
 
