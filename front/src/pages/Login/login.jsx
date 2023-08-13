4ddfd79ab1591b883bd8e0d0f27429a9bfc5eb61
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-08-13 16:43:35 -0300

    Fix: Estilos

--- front/src/pages/Login/login.jsx
+++ front/src/pages/Login/login.jsx
@@ -40,47 +40,46 @@ const UserLogin = () => {
   }
 
   return (
-    <div className="container">
-      <div className="login">
-        <div className="Divimagem">
-          <img src={Logo} className='logo' />
+    <div className="Box">
+      <div className="container">
+        <div className="login">
+          <div className="Divimagem">
+            <img src={Logo} className='logo' />
+          </div>
+          <h1 className="user-login__title">Happy Health</h1>
+          <form className="login-form">
+            <div className="user-login__form-control">
+              <label htmlFor="nome">Usuário</label>
+              <input
+                id="nome"
+                type="text"
+                name="nome"
+                defaultValue={values.nome}
+                onChange={onChange}
+                value={values.nome}
+              />
+            </div>
+            <div className="user-login__form-control">
+              <label htmlFor="senha">Senha</label>
+              <input
+                id="senha"
+                type="password"
+                name="senha"
+                defaultValue={values.senha}
+                onChange={onChange}
+                value={values.senha}
+              />
+            </div>
+            {error && <div className="user-login__error">{error}</div>}
+            <p>Não tem uma conta? <Link to="/create">Cadastre-se aqui</Link></p>
+            <input
+              type={"submit"}
+              value={"enviar"}
+              className="user-login__submit-button"
+              onClick={onSubmit}
+            />
+          </form>
         </div>
-        <h1 className="user-login__title">Happy Health</h1>
-
-        <form className="login-form">
-          <div className="user-login__form-control">
-            <label htmlFor="nome">Usuário</label>
-            <input
-              id="nome"
-              type="text"
-              name="nome"
-              defaultValue={values.nome}
-              onChange={onChange}
-              value={values.nome}
-            />
-          </div>
-          <div className="user-login__form-control">
-            <label htmlFor="senha">Senha</label>
-            <input
-              id="senha"
-              type="password"
-              name="senha"
-              defaultValue={values.senha}
-              onChange={onChange}
-              value={values.senha}
-            />
-          </div>
-          {error && <div className="user-login__error">{error}</div>}
-
-          <p>Não tem uma conta? <Link to="/create">Cadastre-se aqui</Link></p>
-
-          <input
-            type={"submit"}
-            value={"enviar"}
-            className="user-login__submit-button"
-            onClick={onSubmit}
-          />
-        </form>
       </div>
     </div>
   );
