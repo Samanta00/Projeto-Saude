806172ae6f9e15dc5227ce224ece8c690e150651
Ellen Samanta <ellensamanta@uitecnologia.com.br>
2023-08-26 20:36:19 -0300

    feat: nova pagina de conteudo criada

---
+++ front/src/pages/Start/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ConteudoStart from "./conteudo";
+
+
+function Start(props) {
+    return (
+      <>
+        <ConteudoStart/>
+      </>
+  
+    )
+  };
+  
+  export default Start;
