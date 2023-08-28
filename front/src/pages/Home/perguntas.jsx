bc03de3170d50a8afb330123b7e3af0af2425ce2
Ellen Samanta <ellensamanta@uitecnologia.com.br>
2023-08-27 21:06:24 -0300

    fix: adicionado elemento novo em perguntas

--- front/src/pages/Home/perguntas.jsx
+++ front/src/pages/Home/perguntas.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./conteudo.css";
 import aceno from "../../assets/img/acenando.jpg";
 import medicoAceno from "../../assets/img/medicoAcenando.jpg";
+import woman from "../../assets/img/woman-cellphone.jpg";
 
 function Perguntas() {
+  const [activeTab, setActiveTab] = useState(null);
+
+  const handleTabClick = (tabIndex, event) => {
+    event.preventDefault(); // Impede o redirecionamento
+    if (activeTab === tabIndex) {
+      setActiveTab(null); // Fecha a aba se ela já estiver aberta
+    } else {
+      setActiveTab(tabIndex);
+    }
+  };
   return (
     <div className="container__perguntas">
+
+      <div className="womanTeste"><img src={woman} className="womanphoto"></img></div>
       <h2 className="subtitulo-verde">Dúvidas</h2>
 
       <div className="container__perguntas-caixa">
@@ -61,7 +74,72 @@ function Perguntas() {
           </div>
         </div>
       </div>
+      <div>
+
+        <div>
+
+        </div>
+        <div className="container__perguntas-caixa">
+          <div className="container__perguntas-imagem">
+            <img
+              decoding="async"
+              width="800"
+              height="640"
+              src="https://queirozodontologia.com.br/wp-content/uploads/2023/05/2.png"
+              className="attachment-large size-large"
+              alt=""
+              loading="lazy"
+              srcSet="https://queirozodontologia.com.br/wp-content/uploads/2023/05/2.png 992w, https://queirozodontologia.com.br/wp-content/uploads/2023/05/2-300x240.png 300w, https://queirozodontologia.com.br/wp-content/uploads/2023/05/2-768x615.png 768w"
+              sizes="(max-width: 800px) 100vw, 800px"
+            />
+          </div>
+          <div className="container__perguntas-titulo">
+            <div className="elementor-heading-title">
+              Quais são as vantagens desse procedimento?
+            </div>
+            <div
+              id="elementor-tab-title-1981"
+              className="elementor-tab-title"
+              data-tab="1"
+              role="tab"
+              aria-controls="elementor-tab-content-1981"
+              aria-expanded={activeTab === 1}
+              tabIndex="-1"
+              aria-selected={activeTab === 1}
+              onClick={(event) => handleTabClick(1, event)}
+            >
+              <span className="elementor-accordion-icon elementor-accordion-icon-left" aria-hidden="true">
+                <span className="elementor-accordion-icon-closed"><i className="fas fa-plus"></i></span>
+                <span className="elementor-accordion-icon-opened"><i className="fas fa-minus"></i></span>
+              </span>
+              <a className="elementor-accordion-title" href="#">Alívio do estresse e ansiedade</a>
+            </div>
+            {activeTab === 1 && (
+              <div
+                id="elementor-tab-content-1981"
+                className={`elementor-tab-content elementor-clearfix ${activeTab === 1 ? "elementor-active" : ""
+                  }`}
+                data-tab="1"
+                role="tabpanel"
+                aria-labelledby="elementor-tab-title-1981"
+                style={{ display: activeTab === 1 ? "block" : "none" }}
+              >
+                <p>A sedação consciente com óxido nitroso é uma ferramenta eficaz para aliviar o estresse e a ansiedade associados aos tratamentos odontológicos. Ela permite que os pacientes relaxem e esqueçam as preocupações e medos, tornando a experiência no consultório odontológico muito mais tranquila.</p>
+              </div>
+            )}
+          </div>
+        </div>
+
+
+      </div>
     </div>
+
+
+
+
+
+
+
   );
 }
 export default Perguntas;
