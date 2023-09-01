281c741f35cce0723e39ca1b5475135a2ce96c9a
Ellen Samanta <ellensamanta@uitecnologia.com.br>
2023-09-01 13:03:34 -0300

    feat: ajustes

--- front/src/routes/publicRoutes/index.jsx
+++ front/src/routes/publicRoutes/index.jsx
@@ -5,10 +5,11 @@ import PagesHome from '../../pages/Home/home';
 import PagesLogin from '../../pages/Login/login';
 import Cadastro from '../../pages/Cadastro/updateRegistration/form'
 import Quiz from '../../pages/Quiz';
+import Agenda from '../../pages/Agenda';
 
 export const PublicRoutes = () => (
   <React.Fragment>
-    <Route path='/' element={<Start/>}/>
+    <Route path='/' element={<Agenda/>}/>
 
     <Route path="/login" element={<PagesLogin />} />
     <Route path='/create' element={<Cadastro/>}/>
