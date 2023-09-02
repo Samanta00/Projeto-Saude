3b9dcac90854173a1b60ee7195639269931a9e4b
Anderson2A <anderson-asilva@outlook.com>
2023-09-01 21:52:45 -0300

    Feat: refatoração do codigo, alinhamento de pages

--- front/src/pages/Start/conteudo.jsx
+++ front/src/pages/Start/conteudo.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import './style.css';
+import Navbar from '../../component/NavBar';
+import ModalOptions from '../../component/NavBar/ModalOptions'
+
 import seta from './download.svg';
 import seloGarantia from './img/selo-garantia.svg';
 import desconto from './img/desconto.png'
@@ -14,13 +17,8 @@ import cuidadores from './img/cuidadores-50.png';
 const ConteudoStart = () => {
     return (
         <div className='landingPage'>
-            <div>
-                <nav className="navbar navbar-expand-lg" id='navStart'>
-                    
-                    <a className='botao_navbar' href="#">Login</a>
-                    <a className='botao_navbar' href="#">Registre-se</a>
-                </nav>
-            </div>
+            <Navbar />
+            <ModalOptions />
 
             <section className='lp-inicio lp-sc-primario'>
                 <div className='lp-sc-primario__conteudo'>
@@ -34,15 +32,17 @@ const ConteudoStart = () => {
 
                     <a className="lp-sc-primario__botao botao_secundario" href="#">Preço</a>
 
-                    <a className='nossos-precos' href='#row-tabela-preco'><img src={seta} alt="seta" />Preços</a>
+                    <a className='nossos-precos__seta' href='#row-tabela-preco'><img src={seta} alt="seta" />Preços</a>
                 </div>
             </section>
 
-
-            <section className='lp-corpo lp-sc-card'>
+            <section className='container__titulo'>
                 <h2 className='lp-titulo-secundario'>
                     Por que estamos sendo escolhidos
                 </h2>
+            </section>
+
+            <section className='lp-corpo lp-sc-card'>
 
                 <div className='lp-corpo_centro'>
                     <div className="lp-corpo__card">
@@ -54,120 +54,170 @@ const ConteudoStart = () => {
                         </div>
                         <div className="lp-card">
                             <h3>Oferta 2</h3>
-                            <p>Descrição da Oferta 2.</p>
+                            <p>Descrição da Oferta 2.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam dolores, similique voluptatibus repudiandae aspernatur magnam repellendus a odit tenetur. Dolorum neque dolorem recusandae libero quisquam atque laborum reprehenderit inventore! Voluptatem.
+                            </p>
                         </div>
                         <div className="lp-card">
                             <h3>Oferta 3</h3>
-                            <p>Descrição da Oferta 3.</p>
+                            <p>Descrição da Oferta 3.
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia perspiciatis illo totam sequi explicabo veniam magni consectetur quas, cumque atque ratione nulla voluptas modi ducimus adipisci quae temporibus? Itaque, consequatur!
+                            </p>
                         </div>
                     </div>
                 </div>
             </section>
 
 
-            <section className='lp-corpo lp-sc-vantagens'>
-                <h2 class='lp-titulo-secundario'>
+            <section className='container__titulo'>
+                <h2 className='lp-titulo-secundario'>
                     Nossas vantagens
                 </h2>
+            </section>
 
-                <div className="vantagens__corpo">
-                    <div className="card__vantagens">
-                        <img className='card__vantagens_img' src={desconto} alt='desconto' />
-                        <h3>Até 2-3x mais barato que os serviços de reconhecimento manual</h3>
-                        <p>Nenhuma pessoa está envolvida na resolução de captchas.<br />Graças a isso, fornecemos o preço mais baixo do mercado.</p>
-                    </div>
-                </div>
-                <div className="vantagens__corpo">
-                    <div className="card__vantagens">
-                        <img className='card__vantagens_img' src={desconto} alt='desconto' />
-                        <h3>Oferta 1</h3>
-                        <p>Descrição da Oferta 1.</p>
-                    </div>
-                </div>
-                <div className="vantagens__corpo">
-                    <div className="card__vantagens">
-                        <img className='card__vantagens_img' src={desconto} alt='desconto' />
-                        <h3>Oferta 1</h3>
-                        <p>Descrição da Oferta 1.</p>
-                    </div>
-                </div>
+            <section className='lp-corpo lp-sc-vantagens'>
+                <div className='vantagens__corpo__centro'>
 
+                    <div className="vantagens__corpo">
+                        <div className="card__vantagens">
+                            <img className='card__vantagens_img' src={desconto} alt='desconto' />
+                            <h3>Até 2-3x mais barato que os serviços de reconhecimento manual</h3>
+                            <p>Nenhuma pessoa está envolvida na resolução de captchas.<br />Graças a isso, fornecemos o preço mais baixo do mercado.</p>
+                        </div>
+                    </div>
+                    <div className="vantagens__corpo">
+                        <div className="card__vantagens">
+                            <img className='card__vantagens_img' src={desconto} alt='desconto' />
+                            <h3>Oferta 1</h3>
+                            <p>Descrição da Oferta 1.
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque et asperiores optio magnam officiis eaque inventore, perferendis nulla cum fuga quis quisquam ducimus commodi impedit dicta aliquam est maiores numquam.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="vantagens__corpo">
+                        <div className="card__vantagens">
+                            <img className='card__vantagens_img' src={desconto} alt='desconto' />
+                            <h3>Oferta 1</h3>
+                            <p>Descrição da Oferta 1.
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem harum nobis suscipit, rem excepturi commodi autem officiis ex illum labore nesciunt libero illo, molestiae reiciendis, asperiores nemo quae nulla. Sed.
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+
+            <section className='container__titulo'>
+                <h2 className='lp-titulo-secundario'>
+                     Para obter mais ajuda
+                </h2>
             </section>
 
             <section className='lp-corpo lp-sc-ajuda'>
-                <div className='lp-card-ajuda'>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={clinicaParticulares} alt="" />
-                        <h2 class='lp-titulo-secundario'>Clínicas particulares</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={cuidadores} alt="" />
-                        <h2 class='lp-titulo-secundario'>Pacientes e Cuidadores</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={profissionalSaude} alt="" />
-                        <h2 class='lp-titulo-secundario'>Profissionais de Saúde</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={estudantePesquisadores} alt="" />
-                        <h2 class='lp-titulo-secundario'>Estudantes e Pesquisadores</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={ongs} alt="" />
-                        <h2 class='lp-titulo-secundario'>Ongs</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={instituicoesSaude} alt="" />
-                        <h2 class='lp-titulo-secundario'>instituições de Saúde</h2>
-                    </div>
-                    <div class='card__ajuda'>
-                        <img className='card__ajuda-img' src={maletaMedico} alt="" />
-                        <h2 class='lp-titulo-secundario'>Governo e Órgãos de Saúde Pública</h2>
+                <div className='lp-corpo_centro'>
+                    <div className='lp-card-ajuda'>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={clinicaParticulares} alt="" />
+                            <h2 class='lp-titulo-secundario'>Clínicas particulares</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={cuidadores} alt="" />
+                            <h2 class='lp-titulo-secundario'>Pacientes e Cuidadores</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={profissionalSaude} alt="" />
+                            <h2 class='lp-titulo-secundario'>Profissionais de Saúde</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={estudantePesquisadores} alt="" />
+                            <h2 class='lp-titulo-secundario'>Estudantes e Pesquisadores</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={ongs} alt="" />
+                            <h2 class='lp-titulo-secundario'>Ongs</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={instituicoesSaude} alt="" />
+                            <h2 class='lp-titulo-secundario'>instituições de Saúde</h2>
+                        </div>
+                        <div class='card__ajuda'>
+                            <img className='card__ajuda-img' src={maletaMedico} alt="" />
+                            <h2 class='lp-titulo-secundario'>Governo e Órgãos de Saúde Pública</h2>
+                        </div>
                     </div>
                 </div>
+            </section>
+
+            <section className='container__titulo'>
+                <h2 className='lp-titulo-secundario'>
+                    Nosses Preços
+                </h2>
             </section>
 
             <section className='lp-corpo lp-sc-precos'>
-                <div id='row-tabela-preco' className="lp-tabela-precos">
-                    <div className="preco__plano">
-                        <h2>Plano Básico</h2>
-                        <p className="price">$9.99/mês</p>
-                        <ul className="features">
-                            <li>Recursos básicos</li>
-                            <li>Suporte por e-mail</li>
-                        </ul>
-                        <button className="buy-button">Comprar</button>
-                    </div>
-                    <div className="preco__plano">
-                        <h2>Plano Intermediário</h2>
-                        <p className="price">$19.99/mês</p>
-                        <ul className="features">
-                            <li>Recursos avançados</li>
-                            <li>Suporte prioritário</li>
-                        </ul>
-                        <button className="buy-button">Comprar</button>
-                    </div>
-                    <div className="preco__plano">
-                        <h2>Plano Premium</h2>
-                        <p className="price">$29.99/mês</p>
-                        <ul className="features">
-                            <li>Todos os recursos</li>
-                            <li>Suporte 24/7</li>
-                        </ul>
-                        <button className="buy-button">Comprar</button>
+                <div className='lp-tabela'>
+
+                    <div id='row-tabela-preco' className="lp-tabela-precos">
+                        <div className="preco__plano">
+                            <p className='price__titulo'>Plano Premium</p>
+                            <p className="price__preco">$29.99/mês</p>
+                            <ul className="features__preco">
+                                <li>Todos os recursos</li>
+                                <li>Suporte 24/7</li>
+                            </ul>
+                            <button className="buy-button__preco">Comprar</button>
+                        </div>
+                        <div className="preco__plano">
+                            <p className='price__titulo'>Plano Premium</p>
+                            <p className="price__preco">$29.99/mês</p>
+                            <ul className="features__preco">
+                                <li>Todos os recursos</li>
+                                <li>Suporte 24/7</li>
+                            </ul>
+                            <button className="buy-button__preco">Comprar</button>
+                        </div>
+                        <div className="preco__plano">
+                            <p className='price__titulo'>Plano Premium</p>
+                            <p className="price__preco">$29.99/mês</p>
+                            <ul className="features__preco">
+                                <li>Todos os recursos</li>
+                                <li>Suporte 24/7</li>
+                            </ul>
+                            <button className="buy-button__preco">Comprar</button>
+                        </div>
                     </div>
                 </div>
             </section>
 
-            <section className='lp-corpo lp-sc-garantia'>
+
+            <section className='container__titulo'>
+                <h2 className='lp-titulo-secundario'>
+                    Selo de Garantia
+                </h2>
                 <img src={seloGarantia} alt="seta" />
-                <h2>Garantia</h2>
-                <p>Se você não gosta da qualidade dos serviços prestados pelo serviço, Garantimos-lhe um reembolso sem explicação no prazo de 3 dias a partir da data da compra.</p>
+                <p className='garantia__texto'>Se você não gosta da qualidade dos serviços prestados pelo serviço, Garantimos-lhe um reembolso sem explicação no prazo de 3 dias a partir da data da compra.</p>
             </section>
 
-            <footer>
-                <p>Comece economizando até 90% do seu orçamento sobre o reconhecimento de captchas com CapMonster.cloud</p>
-                <button className="buy-button">Teste Agora</button>
+            <footer className='rodape__lp'>
+                <p className='rodape__lp__texto'>Fale Conosco
+                    E-mail:
+                    contato@queirozodontologia.com.br
+                </p>
+
+                <p className='rodape__lp__texto'>
+                    Telefone
+                    (81) 3461.3835
+                </p>
+                <p className='rodape__lp__texto'>
+                    WhatsApp
+                    (81) 99677-9647
+                </p>
+                <p className='rodape__lp__texto'>
+                    Excelente localização
+                    Edifício Pontes Corporate Center, R. Barão de Souza Leão, 425 Salas 504/505 - Boa Viagem, Recife - PE
+                </p>
+
+                <button className="buy-button__preco footer-button">Teste Agora</button>
             </footer>
         </div>
     );
