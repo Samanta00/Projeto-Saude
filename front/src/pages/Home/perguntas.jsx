04f9a4fe6890f950f87c59acec624fa58856887a
Anderson2A <anderson-asilva@outlook.com>
2023-09-03 13:06:51 -0300

    Fix: bugs concertados e alinhados

--- front/src/pages/Home/perguntas.jsx
+++ front/src/pages/Home/perguntas.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./conteudo.css";
+import Carousel from './carousel';
 import aceno from "../../assets/img/acenando.jpg";
 import servicos from "../../assets/img/euu.jpeg";
 import woman from "../../assets/img/euu.jpeg";
@@ -7,6 +8,7 @@ import clinica from "../../assets/img/clinica.png";
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous"></link>
 
 function Perguntas() {
+
   const [activeTab, setActiveTab] = useState(null);
 
   const handleTabClick = (tabIndex, event) => {
@@ -18,7 +20,7 @@ function Perguntas() {
     }
   };
   const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [woman, woman, woman]; // Substitua com suas imagens
+  const images = [woman, clinica, woman]; // Substitua com suas imagens
 
   const goToPreviousSlide = () => {
     setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
@@ -30,9 +32,10 @@ function Perguntas() {
   return (
     <div className="container__perguntas">
       <div className="womanTeste">
-        <img src={woman} className="womanphoto" alt="" />
+
+      <img src={woman} className="womanphoto" alt="" />
+        <h2 className="subtitulo-verde">Dúvidas</h2>
       </div>
-      <h2 className="subtitulo-verde">Dúvidas</h2>
 
       <div className="container__perguntas-caixa">
         <div className="container__perguntas-imagem">
@@ -48,10 +51,12 @@ function Perguntas() {
             sizes="(max-width: 800px) 100vw, 800px"
           />
         </div>
+
         <div className="container__perguntas-titulo">
           <div className="elementor-heading-title">
             Quais são as vantagens desse procedimento?
           </div>
+
           <div
             id="elementor-tab-title-1981"
             className={`elementor-tab-title ${activeTab === 1 ? "elementor-active" : ""
@@ -73,10 +78,10 @@ function Perguntas() {
               <i className="fas fa-minus"></i>
             </span>
             <a className="elementor-accordion-title" href="#">
-            Sofro com A doença, Como o Sistema Poderia me Ajudar?
-
+              Sofro com A doença, Como o Sistema Poderia me Ajudar?
             </a>
           </div>
+
           {activeTab === 1 && (
             <div
               id="elementor-tab-content-1981"
@@ -99,22 +104,25 @@ function Perguntas() {
               </p>
             </div>
           )}
+
+            {/* ... previous code  Acordeon... */}
+            
           <div className="container__perguntas-titulo">
 
             <div
-              id="elementor-tab-title-1981"
-              className={`elementor-tab-title ${activeTab === 1 ? "elementor-active" : ""
+              id="elementor-tab-title-1982"
+              className={`elementor-tab-title ${activeTab === 2 ? "elementor-active" : ""
                 }`}
-              data-tab="1"
+              data-tab="2"
               role="tab"
-              aria-controls="elementor-tab-content-1981"
-              aria-expanded={activeTab === 1}
+              aria-controls="elementor-tab-content-1982"
+              aria-expanded={activeTab === 2}
               tabIndex="-1"
-              aria-selected={activeTab === 1}
-              onClick={(event) => handleTabClick(1, event)}
+              aria-selected={activeTab === 2}
+              onClick={(event) => handleTabClick(2, event)}
             >
               <span
-                className={`elementor-accordion-icon ${activeTab === 1
+                className={`elementor-accordion-icon ${activeTab === 2
                   ? "elementor-accordion-icon-opened"
                   : "elementor-accordion-icon-closed"
                   }`}
@@ -122,18 +130,18 @@ function Perguntas() {
                 <i className="fas fa-minus"></i>
               </span>
               <a className="elementor-accordion-title" href="#">
-                Não sofro com A doença, Como o Sistema Poderia me Ajudar?
+              Não sofro com A doença, Como o Sistema Poderia me Ajudar?
               </a>
             </div>
-            {activeTab === 1 && (
+            {activeTab === 2 && (
               <div
-                id="elementor-tab-content-1981"
-                className={`elementor-tab-content elementor-clearfix ${activeTab === 1 ? "elementor-active" : ""
+                id="elementor-tab-content-1982"
+                className={`elementor-tab-content elementor-clearfix ${activeTab === 2 ? "elementor-active" : ""
                   }`}
-                data-tab="1"
+                data-tab="2"
                 role="tabpanel"
-                aria-labelledby="elementor-tab-title-1981"
-                style={{ display: activeTab === 1 ? "block" : "none" }}
+                aria-labelledby="elementor-tab-title-1982"
+                style={{ display: activeTab === 2 ? "block" : "none" }}
               >
                 <p>
                   O Projeto de Software tras diversas formas educacionais e
@@ -143,99 +151,52 @@ function Perguntas() {
                 </p>
               </div>
             )}
-          </div>
 
-          {/* Segundo acordeão */}
-          <div className="container__perguntas-titulo">
+                {/* ... previous code  Acordeon... */}
 
-            <div
-              id="elementor-tab-title-1982"
-              className={`elementor-tab-title ${activeTab === 2 ? "elementor-active" : ""
-                }`}
-              data-tab="2"
-              role="tab"
-              aria-controls="elementor-tab-content-1982"
-              aria-expanded={activeTab === 2}
-              tabIndex="-1"
-              aria-selected={activeTab === 2}
-              onClick={(event) => handleTabClick(2, event)}
-            >
-              <span
-                className={`elementor-accordion-icon ${activeTab === 2
-                  ? "elementor-accordion-icon-opened"
-                  : "elementor-accordion-icon-closed"
-                  }`}
-              >
-                <i className="fas fa-minus"></i>
-              </span>
-              <a className="elementor-accordion-title" href="#">
-                Título do segundo acordeão
-              </a>
-            </div>
-            {activeTab === 2 && (
+            <div className="container__perguntas-titulo">
+
               <div
-                id="elementor-tab-content-1982"
-                className={`elementor-tab-content elementor-clearfix ${activeTab === 2 ? "elementor-active" : ""
+                id="elementor-tab-title-1982"
+                className={`elementor-tab-title ${activeTab === 2 ? "elementor-active" : ""
                   }`}
                 data-tab="2"
-                role="tabpanel"
-                aria-labelledby="elementor-tab-title-1982"
-                style={{ display: activeTab === 2 ? "block" : "none" }}
+                role="tab"
+                aria-controls="elementor-tab-content-1982"
+                aria-expanded={activeTab === 2}
+                tabIndex="-1"
+                aria-selected={activeTab === 2}
+                onClick={(event) => handleTabClick(2, event)}
               >
-                <p>
-                  Conteúdo do segundo acordeão.
-                </p>
+                <span
+                  className={`elementor-accordion-icon ${activeTab === 2
+                    ? "elementor-accordion-icon-opened"
+                    : "elementor-accordion-icon-closed"
+                    }`}
+                >
+                  <i className="fas fa-minus"></i>
+                </span>
+                <a className="elementor-accordion-title" href="#">
+                  Título do Terceiro acordeão
+                </a>
               </div>
-            )}
-          </div>
-          {/* ... previous code ... */}
-
-          {/* Third acordeão */}
-          <div className="container__perguntas-titulo">
-
-            <div
-              id="elementor-tab-title-1982"
-              className={`elementor-tab-title ${activeTab === 2 ? "elementor-active" : ""
-                }`}
-              data-tab="2"
-              role="tab"
-              aria-controls="elementor-tab-content-1982"
-              aria-expanded={activeTab === 2}
-              tabIndex="-1"
-              aria-selected={activeTab === 2}
-              onClick={(event) => handleTabClick(2, event)}
-            >
-              <span
-                className={`elementor-accordion-icon ${activeTab === 2
-                  ? "elementor-accordion-icon-opened"
-                  : "elementor-accordion-icon-closed"
-                  }`}
-              >
-                <i className="fas fa-minus"></i>
-              </span>
-              <a className="elementor-accordion-title" href="#">
-                Título do terceiro acordeão
-              </a>
+              {activeTab === 2 && (
+                <div
+                  id="elementor-tab-content-1982"
+                  className={`elementor-tab-content elementor-clearfix ${activeTab === 2 ? "elementor-active" : ""
+                    }`}
+                  data-tab="2"
+                  role="tabpanel"
+                  aria-labelledby="elementor-tab-title-1982"
+                  style={{ display: activeTab === 2 ? "block" : "none" }}
+                >
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore ducimus, voluptatibus nemo ad dolore vitae praesentium ipsa sapiente facere reprehenderit odit possimus accusantium maxime animi quia, error provident architecto eum!
+                  </p>
+                </div>
+              )}
             </div>
-            {activeTab === 2 && (
-              <div
-                id="elementor-tab-content-1982"
-                className={`elementor-tab-content elementor-clearfix ${activeTab === 2 ? "elementor-active" : ""
-                  }`}
-                data-tab="2"
-                role="tabpanel"
-                aria-labelledby="elementor-tab-title-1982"
-                style={{ display: activeTab === 2 ? "block" : "none" }}
-              >
-                <p>
-                  Conteúdo do terceiro acordeão.
-                </p>
-              </div>
-            )}
           </div>
-
-
-
         </div>
 
       </div>
@@ -306,9 +267,15 @@ function Perguntas() {
       </section>
 
 
-      <div className="component-wrapper">
-        <div className="subcomponents-container">
-          <div className="subcomponent">
+      <div className="component-wrapper ">
+        <div className=" component-especialista">
+          <div className="subcomponent subcomponent-especialista">
+            <img src={servicos} className="servicos" alt="" />
+            <h2>Ceo Fundador</h2>
+            <h1>Reynaldo Gianecchini</h1>
+          </div>
+
+          <div className="subcomponent subcomponent-especialista">
             <img src={servicos} className="servicos" alt="" />
             <h2>Ceo Fundador</h2>
             <h1>Reynaldo Gianecchini</h1>
@@ -316,14 +283,8 @@ function Perguntas() {
         </div>
 
       </div>
-
-      <div>
-      <div>
-        <img className="d-block w-100" src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
-      </div>
-      <button onClick={goToPreviousSlide}>Previous</button>
-      <button onClick={goToNextSlide}>Next</button>
-    </div>
+                
+      <Carousel images={images} />
 
       <div className="entreEmContato">
         <div className="contact-info">
