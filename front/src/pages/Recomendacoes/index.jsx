040d36046edae11299bd2920caa1c4183af0c7c6
Ellen Samanta <ellen.samanta@outlook.com>
2023-09-10 20:28:57 -0300

    fix: organização do conteudo de recomendações

---
+++ front/src/pages/Recomendacoes/index.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+import Navbar from "../../component/NavBar";
+import ModalOptions from "../../component/NavBar/ModalOptions";
+import Conteudo from "./paginas/Conteudo";
+
+const Index = () => {
+
+  return (
+    <>
+      <Navbar />
+      <ModalOptions />
+      <Conteudo/>
+
+    </>
+  );
+};
+
+export default Index;
