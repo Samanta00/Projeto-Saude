4962172b68bd9058ee3db41929dd8b6992910162
Anderson2A <112587285+Anderson-A2ilva@users.noreply.github.com>
2023-10-27 12:06:10 -0300

    Fix: quiz and add a new routes

---
+++ front/src/pages/quiz 2.0/quizNavegation.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import Navbar from '../../component/NavBar';
+import Quiz2 from './quiz/quizteste'
+
+export default function Quiz() {
+ 
+  return (
+    <>
+      <Navbar />
+      <Quiz2/>   
+    </>
+  );
+}
--- front/src/routes/publicRoutes/index.jsx
+++ front/src/routes/publicRoutes/index.jsx
@@ -4,17 +4,16 @@ import Start from '../../pages/Start';
 import PagesHome from '../../pages/Home/home';
 import PagesLogin from '../../pages/Login/login';
 import Cadastro from '../../pages/Cadastro/updateRegistration/form'
-import Quiz from '../../pages/Quiz';
+import Quiz from '../../pages/quiz 2.0/quizNavegation';
 import Agenda from '../../pages/Agenda';
 import Recomendacoes from '../../pages/Recomendacoes/index.jsx';
 import Resultados from '../../pages/Resultados';
 import PerguntasQuiz from '../../pages/Quiz/index';
-import QuizTeste from '../../pages/quiz 2.0/quizteste';
 
 export const PublicRoutes = () => (
   <React.Fragment>
-    <Route path='/' element={<Recomendacoes/>}/>
-     <Route path='/start' element={<Start/>}/>
+    <Route path='/' element={<Quiz/>}/>
+    <Route path='/start' element={<Start/>}/>
     <Route path='/home' element={<PagesHome/>}/>
     <Route path='/quiz' element={<Quiz/>}/>
     <Route path='/agendamento' element={<Agenda/>}/>
@@ -22,6 +21,7 @@ export const PublicRoutes = () => (
     <Route path="/login" element={<PagesLogin />} />
     <Route path='/cadastro' element={<Cadastro/>}/>
     <Route path='/perguntas' element={<PerguntasQuiz/>}/>
+    <Route path='/Recomendacoes' element={<Recomendacoes/>}/>
   </React.Fragment>
 )
 
